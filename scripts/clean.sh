4b3957b7c70a9c3635db994b12784be02272c7dd
Ankit <as.dummy.git@gitdummies.superfake>
2014-02-12 15:34:02 -0600

    clean.sh now removes resultfiles and pathlength files

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -4,7 +4,8 @@
 ###  
 
 rm -rf ../flowIDmap ../linkCaps*
-rm -rf ../topology/*lp ../topology/*mcf topology/path*/*
+rm -rf ../topology/*lp ../topology/*mcf ../topology/path*/*
 rm -rf ../plots/*pdf
 rm -rf ../lpmaker/*.class ../lpmaker/graphs/*.class
 rm -rf flowtmp*  gurobi.log runfile templog
+rm -rf resultfiles/*
